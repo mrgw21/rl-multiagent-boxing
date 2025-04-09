d814839aeea2cf8b78aa1ae50970d2313f6e1647
glawless1 <lawless.george@yahoo.com>
2025-04-09 16:44:11 +0100

    Not running, needs adjustments

--- training/ppo.py
+++ training/ppo.py
@@ -8,7 +8,6 @@ At each transistion we need to collect:
 - rewards
 - value estimates
 
-
 """
 
 import math
@@ -17,6 +16,8 @@ import random
 import gymnasium as gym
 import ale_py
 import numpy as np
+import torch
+import neural_ne
 
 # Hyperparamaters
 # --------------------------------------------------------------------
@@ -25,10 +26,11 @@ class PPOAgent:
     
     def __init__ (self):
         
-        GAMMA = 0.99 
-        CLIP_EPSILON = 0.2
-        ENTROPY_COEF = 0.01
-        
+        self.gamma = 0.99 
+        self.clip_epsi = 0.2
+        self.entropy = 0.01
+        self.actor = neural_ne.Actor()
+        self.critic = neural_ne.Critic()
         
         self.information = {
                 'state': [],
@@ -39,25 +41,46 @@ class PPOAgent:
                 'cumulative_reward' : 0
             }
     
-    def updateInformation (self, new_state, reward, done, trunc, info, action):
-        self.information['state'].append(new_state)
-        self.information['state_value_function'].append('NN_state')
+    def updateInformation (self, state, reward, done, trunc, info, action):
+        """Updates information from the action taken e.g., new states, rewards"""
+        self.information['state'].append(state)
+        self.information['state_value_function'].append(self.critic.forward(state))
         self.information['action'].append(action)
-        self.information['log_prob_action'].append('log this action')
+        self.information['log_prob_action'].append('log_prob(action)')
         self.information['reward'].append(reward)
         self.information['cumulative_reward'] += reward
         
     def reset_information (self):
-                self.information = {
-                'state': [],
-                'state_value_function': [],
-                'action': [],
-                'log_prob_action': [],
-                'reward': [],
-                'cumulative_reward' : 0
-            }
+        """Resets information when the learning experience is over, ready for another one."""
+        self.information = {
+        'state': [],
+        'state_value_function': [],
+        'action': [],
+        'log_prob_action': [],
+        'reward': [],
+        'cumulative_reward' : 0
+    }
     
-    def log_prob (self, action_probability):
-        return 
-                
+    def log_prob(action_probability):
+        return math.log(action_probability)
     
+    def compute_advantages (self, timestamp, state_value_function):
+        """Computes advantages the long way"""
+        rewards_after_state = self.information['reward'][timestamp:]
+        return (sum([rewards_after_state[x] ^ (x+1) for x in range(len(rewards_after_state))]) - state_value_function)
+    
+    # This needs some more work
+    def compute_gen_advantage_estimation (self, current_state, next_state, reward):
+        """Uses bellman equation to calculate advantage estimate"""
+        current_state_value, next_state_value = self.critic.forward(current_state), self.critic.forward(next_state)
+        return (reward + (self.gamma * next_state_value) - current_state_value)
+    
+    
+    def clipping(self, advantage, old_probability, new_probability):
+        """Calculates the clipped value"""
+        ratio = new_probability/old_probability
+        return min(torch.clamp(ratio * advantage), torch.clamp(ratio, 1-self.clip_epsi, 1+self.clip_epsi) * advantage)
+    
+    def get_action (self, state):
+        """Gets action from actor NN"""
+        return self.actor.forward(state)
--- training/ppo_main.py
+++ training/ppo_main.py
@@ -3,7 +3,7 @@ import random
 import gymnasium as gym
 import ale_py
 import numpy as np
-
+import ppo
 
 
 env = gym.make("ALE/Boxing-v5", render_mode="human")
@@ -14,52 +14,30 @@ for x in range(1000):
     action = env.action_space.sample() # take a random action
     observation, reward, done, trunc, info = env.step(action)
     print(x)
-    
-#actor = neural_ne.Actor
-#critic = neural_ne.Critic
 
+agent = ppo.PPOAgent()
 
 def gather_data():
+    """Gathers data for a full episode"""
+    done = False
+    state = env.reset()
+    agent = ppo.PPOAgent()
 
-    for x in range(100):
-        
-        done = False
-        state = env.reset()
-        
-        while not done:
+    while not done:
     
-            information = {
-                'state': [],
-                'state_value_function': [],
-                'action': [],
-                'log_prob_action': [],
-                'reward': [],
-                'cumulative_reward' : 0
-            }
+        # Retrieves the action to be taken
+        action = agent.get_action(state)
+        
+        # Executes function and retrieves information
+        new_state, reward, done, trunc, info = env.step(action)
+        
+        # Updates the information for that timestamp into the information dictionary
+        agent.updateInformation(state, reward, done, trunc, info, action)
+        
+        state = new_state
             
-            done = False
-            while not done:
-            
-                # Retrieves the action to be taken
-                #NN_action = actor.compute()
-                
-                # Computes the value function of the state
-                #NN_state = critic.compute()
-                
-                new_state, reward, done, trunc, info = env.step(action)
-                
-                # Updates the information for that timestamp into the information dictionary
-                information['state'].append(state)
-                information['state_value_function'].append('NN_state')
-                information['action'].append(action)
-                information['log_prob_action'].append('log this action')
-                information['reward'].append(reward)
-                information['cumulative_reward'] += reward
-                
-                state = new_state
-            
-            
-            
+
+         
             
             
             
